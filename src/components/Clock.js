1294fa5b0a4578cc2bd8d83ab58db705d991a94a
imimran <engr.aih@gmail.com>
2021-07-24 16:16:14 +0600

    React Life Circle example

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
 class Clock extends React.PureComponent {
+    state = { date: new Date() };
+
+    componentDidMount() {
+        this.clockTimer = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    handleClick() {
+        console.log('Button clicked');
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
+    }
+
     render() {
-        const { locale } = this.props;
+        const { date } = this.state;
         return (
-            <h1 className="heading">
-                <span className="text">{new Date().toLocaleDateString(locale)}</span>
-            </h1>
+            <div>
+                <h1 className="heading">
+                    <span className="text">{date.toLocaleDateString('bn-BD')}</span>
+                </h1>
+                <button type="button" onClick={this.handleClick}>
+                    Click Here
+                </button>
+            </div>
         );
     }
 }
